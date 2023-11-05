006cb13ef9b9e6449da3a74fa2c50e26ebc9a365
RutujaHatgine <rutujahatgine2000@gmail.com>
2023-11-05 08:26:50 +0530

    assignmentno5_4

---
+++ assignmentno5_4/src/com/sunbeam/TesterMain.java
@@ -0,0 +1,15 @@
+package com.sunbeam;
+
+public class TesterMain 
+{
+	public static void main(String[] args) {
+		BSTree root=new BSTree();
+		root.add(10);
+		root.add(20);
+		root.add(35);
+		root.add(30);
+		root.add(50);
+		
+		root.display();
+	}
+}
